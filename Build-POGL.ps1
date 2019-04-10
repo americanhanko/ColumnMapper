1920b2f273546b857289caa8716cbd6605aeb908
Eric Hanko <eric.hanko1@gmail.com>
2019-04-10 13:01:31 -0700

    remove comments since we have a test file

--- Build-POGL.ps1
+++ Build-POGL.ps1
@@ -22,22 +22,3 @@ function Get-POGLHash ($csv)
   }
   $hash
 }
-
-# [PO] PO Id                                        : EP64668
-# [PO] Order Id                                     : 8100089500
-# [PO]Ordered Date (Date)                           : 3/11/2019
-# [PO]Requester (User)                              : Cara Larson
-# [PO]GL Account (GL Account Id)                    : 651000
-# [PO]Supplier (ERP Supplier)                       : BUNZL RETAIL SERVICES
-# [PO]GL Account (GL Account Name)                  : Office Supplies
-# [PO]Cost Center (Cost center ID)                  : 15
-# [PO]Cost Center (Cost Center)                     : BLOOMINGTON
-# [PO]Purchasing Company (Purchase Organization Id) : 1000
-# [RCT]Date Received (Year)                         : 2019
-# [RCT]Date Received (Quarter)                      : Q1
-# [RCT]Date Received (Month)                        : Mar
-# [RCT]Date Received (Date)                         : 3/19/2019
-# sum(Amount Invoiced)                              : 0
-# sum(Amount Left to Reconcile Total)               : 65.04
-# sum(Amount Accepted)                              : 0
-# sum(PO Spend)                                     : 65.04
