233a614761f1d66528db01460df7880f6df79295
Eric Hanko <eric.hanko1@gmail.com>
2019-04-10 13:09:02 -0700

    I broke something

--- Build-POGL.Tests.ps1
+++ Build-POGL.Tests.ps1
@@ -1,15 +1,14 @@
-$here = (Split-Path -Parent $MyInvocation.MyCommand.Path)
-. $here\Build-POGL.ps1
+Import-Module -Name ./Build-POGL.ps1
 
 Describe 'Get-CsvObjects' {
     It 'imports data without error' {
-      $data = Get-CsvObjects './raw_test.csv'
+      $data = Get-CsvObjects './tests/raw_test.csv'
       $data[0]."[PO] Order Id" | Should -Be '8100000630'
     }
 }
 
 Describe 'Get-POGLHash' {
   It 'creates a hash even with duplicate keys' {
-    $data = Get-POGLHash './raw_test.csv'
+    $data = Get-POGLHash './tests/raw_test.csv'
   }
 }
